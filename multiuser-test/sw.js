a9e1ce110fa9f97fdd6f3cb8a8cf25d150848997
hubert00741 <hubert00741@gmail.com>
2026-09-02 12:13:30 +0200

    Add files via upload

--- multiuser-test/sw.js
+++ multiuser-test/sw.js
@@ -1,4 +1,4 @@
-const CACHE='gymlog-crush-multiuser-test-v0718';
+const CACHE='gymlog-crush-multiuser-test-v0719';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-512-maskable.png','./apple-touch-icon.png','./header-logo.png','./header-logo-female.png'];
 
 self.addEventListener('install',e=>{
